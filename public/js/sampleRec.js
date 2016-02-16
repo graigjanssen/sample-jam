7c71d2e25a408ca0c1a5a6ef782863b176b44034
Graig Janssen <graig.janssen@gmail.com>
2016-02-16 14:50:28 -0500

    Hidden forms with audio data generated for each sample recording

--- public/js/sampleRec.js
+++ public/js/sampleRec.js
@@ -21,16 +21,43 @@ var WavReader = {
     reader.readAsDataURL(file);
   }
 };
+
 function sendToSampler(){
   var samplerModule = $('#mod-menu').val();
   audioRecorder.exportWAV(function(blob){
     blob.name = samplerModule;
-    WavReader.read(blob, function(err, fileInfo){
-      console.log('fileInfo from WavReader', fileInfo);
-    });
     var url = URL.createObjectURL(blob);
+    $(samplerModule).empty();
     $(samplerModule).attr('src', url);
+
+    // Get object with file info including base64 string of audio data, append as hidden fields //
+    WavReader.read(blob, function(err, fileInfo){
+      $(samplerModule).append(createHiddenFields(fileInfo));
+    });
   });
+}
+
+function createHiddenFields(fileInfo){
+  var $form = $('<form>');
+
+  var $name = $('<input>').attr({
+    type: 'hidden',
+    name: 'name'
+  }).val(fileInfo.name);
+  var $type = $('<input>').attr({
+    type: 'hidden',
+    name: 'type'
+  }).val(fileInfo.type);
+  var $size = $('<input>').attr({
+    type: 'hidden',
+    name: 'size'
+  }).val(fileInfo.size);
+  var $file = $('<input>').attr({
+    type: 'hidden',
+    name: 'file'
+  }).val(fileInfo.file);
+
+  return $form.append([$name, $type, $size, $file]);
 }
 
 function newRecording(){
