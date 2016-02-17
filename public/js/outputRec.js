ddc4f6b3b3051d0e24bf2ffa4b48b79c4182eb72
Graig Janssen <graig.janssen@gmail.com>
2016-02-16 19:19:20 -0500

    Adds style to user features section

--- public/js/outputRec.js
+++ public/js/outputRec.js
@@ -2,12 +2,22 @@
 function appendPlaybackElement(){
   samplerRecorder.exportWAV(function(blob){
     var url = URL.createObjectURL(blob);
-    var $li = $('<li>');
+    var $li = $('<li>').addClass('playlist-item');
+    // Create and append audio element //
     var $au = $('<audio>').attr({
       src: url,
-      controls: true
+      controls: true,
+      class: 'playlist-audio'
     });
     $li.append($au);
+
+    // Create and append download link //
+    var $link = $('<a>').attr({
+      href: url,
+      download: 'myJam'
+    });
+    $link.html("<button class='btn download-btn'>Download</button>");
+    $li.append($link);
     $('#playlist').append($li);
   });
 }
